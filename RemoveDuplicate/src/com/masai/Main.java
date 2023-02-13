93d47082a7895f6bebe87f677adddb3f3888e80e
efat56 <104213039+efat56@users.noreply.github.com>
2023-02-13 22:15:28 +0530

    done

--- RemoveDuplicate/src/com/masai/Main.java
+++ RemoveDuplicate/src/com/masai/Main.java
@@ -4,6 +4,17 @@ import java.util.LinkedHashSet;
 
 public class Main {
 	
+	public static void reverseString(String str) {
+		char[] c = str.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=c.length-1; i>=0; i--) {
+			sb.append(c[i]);
+			
+		}
+		System.out.println(sb);
+	}
+	
 	public static String removeDuplicateFromString(String original) {
 		
 		char[] chr= original.toCharArray();
@@ -36,9 +47,12 @@ public static void main(String[] args) {
 			
 
 	
-	String s= Main.removeDuplicateFromString("Apple");
+	//String s= Main.removeDuplicateFromString("Apple");
 	
-	System.out.println(s);
+	//System.out.println(s);
+	
+	
+	Main.reverseString("efat");
 
 
 }
